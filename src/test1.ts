fe092c403c9481f7329cdcc9fcf22db7608211f6
Sunny4123 <151112974+Sunny4123@users.noreply.github.com>
2024-10-04 13:36:34 +0700

    test1

--- src/test1.ts
+++ src/test1.ts
@@ -1,8 +1,34 @@
+import axios from "axios";
 import { Utils } from "./Utils";
 
-if(Utils.add(1,2) === 3)
-{
-    console.log(0)
-}else{
-    console.log(1)
+
+const unit_test = async() => {
+
+    if (Utils.add(1, 2) === 3) {
+        console.log(0)
+    } else {
+        console.log(1)
+    }
+
+    const data: any = {
+        "name": "pop",
+        "username": "iampop"
+    }
+    try{
+        const response: any = await Utils.addUser(data);
+        const correct_result: any = { "name": "pop", "username": "iampop" };
+    
+        const keys:string[] = Object.keys(correct_result);
+        for(const k of keys){
+            if(correct_result[k] !== response[k]){
+                console.log("addUser" + k)
+                return;
+            }
+        }
+    }catch(error){
+        console.log("addUser" + error);
+    }
+   
+
 }
+unit_test();
