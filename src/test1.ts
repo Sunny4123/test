29cbced214529a822c42024961de2ee074760964
Sunny4123 <151112974+Sunny4123@users.noreply.github.com>
2024-10-09 01:56:45 +0700

    test123456

--- src/test1.ts
+++ src/test1.ts
@@ -1,34 +1,45 @@
-import axios from "axios";
 import { Utils } from "./Utils";
 
+// if(Utils.add(1,2) === 6)
+// {
+//     console.log('Test passed: 1 + 1 = 2'); // Success message
+// }else{
+//     console.log('Test failed: 1 + 1); // Failure message');
+// }
 
-const unit_test = async() => {
+// test.js
 
-    if (Utils.add(1, 2) === 3) {
-        console.log(0)
-    } else {
-        console.log(1)
-    }
 
-    const data: any = {
-        "name": "pop",
-        "username": "iampop"
-    }
-    try{
-        const response: any = await Utils.addUser(data);
-        const correct_result: any = { "name": "pop", "username": "iampop" };
-    
-        const keys:string[] = Object.keys(correct_result);
-        for(const k of keys){
-            if(correct_result[k] !== response[k]){
-                console.log("addUser" + k)
-                return;
-            }
-        }
-    }catch(error){
-        console.log("addUser" + error);
-    }
-   
+let allTestsPassed = true;
 
+if (Utils.calculate(1, 1, '+') === 2) {
+    console.log('Test passed: 1 + 1 = 2'); // Success message
+} else {
+    console.log('Test failed: 1 + 1'); // Failure message
+    allTestsPassed = false;
 }
-unit_test();
+
+if (Utils.calculate(5, 3, '-') === 2) {
+    console.log('Test passed: 5 - 3 = 2');
+} else {
+    console.log('Test failed: 5 - 3');
+    allTestsPassed = false;
+}
+
+if (Utils.calculate(3, 4, '*') === 12) {
+    console.log('Test passed: 3 * 4 = 12');
+} else {
+    console.log('Test failed: 3 * 4');
+    allTestsPassed = false;
+}
+
+if (Utils.calculate(10, 2, '/') === 5) {
+    console.log('Test passed: 10 / 2 = 5');
+} else {
+    console.log('Test failed: 10 / 2');
+    allTestsPassed = false;
+}
+
+if (!allTestsPassed) {
+    process.exit(1); // Exit with a failure code if any test failed
+}
